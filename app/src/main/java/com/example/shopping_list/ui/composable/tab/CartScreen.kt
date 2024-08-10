ee5d2f52e614bf09f094309002c9aec4a2a08b31
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-08-10 01:07:09 -0300

    feat: add Grid Layout for products

--- app/src/main/java/com/example/shopping_list/ui/composable/tab/CartScreen.kt
+++ app/src/main/java/com/example/shopping_list/ui/composable/tab/CartScreen.kt
@@ -1,18 +1,19 @@
 package com.example.shopping_list.ui.composable.tab
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
 
-
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -29,21 +30,19 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.rememberAsyncImagePainter
 import com.example.shopping_list.R
 import com.example.shopping_list.model.Product
 import com.example.shopping_list.ui.theme.Blue40
 
 @Composable
 fun CartScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(
-            text = "Cart",
-            color = Color.Blue,
-        )
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(bottom = 88.dp)
+    ) {
+        ProductGridPreview()
     }
 }
-
 
 @Composable
 fun ProductCard(
@@ -54,15 +53,14 @@ fun ProductCard(
         shape = RoundedCornerShape(8.dp),
         elevation = CardDefaults.cardElevation(4.dp),
         modifier = Modifier
-            .height(270.dp)
-            .width(213.dp)
             .fillMaxWidth()
-            .wrapContentWidth(),
+            .height(270.dp),
         colors = CardDefaults.cardColors(
             containerColor = Color.White,
         )
     ) {
-        Column(modifier = Modifier.fillMaxSize()
+        Column(modifier = Modifier
+            .fillMaxSize()
             .padding(start = 15.dp, end = 15.dp, top = 20.dp)) {
             Image(
                 painter = painterResource(id = R.drawable.sample_image),
@@ -73,25 +71,25 @@ fun ProductCard(
                     .width(100.dp),
                 contentScale = ContentScale.Fit
             )
-            Spacer(modifier = Modifier.height(8.dp))
             Text(
                 text = product.title,
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(top = 16.dp),
+                maxLines = 1
             )
-            Spacer(modifier = Modifier.height(4.dp))
             Text(
                 text = "Precio : $${product.price}",
                 fontSize = 16.sp,
                 fontWeight = FontWeight.Medium,
                 modifier = Modifier.padding(top = 16.dp)
             )
-            Spacer(modifier = Modifier.height(16.dp))
             Button(
                 onClick = onBuyClick,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .align(Alignment.CenterHorizontally),
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 16.dp),
 
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Blue40,
@@ -120,3 +118,79 @@ fun ProductCardPreview() {
     )
 }
 
+@Composable
+fun ProductGrid(
+    products: List<Product>,
+    onBuyClick: (Product) -> Unit
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(150.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(products) { product ->
+            ProductCard(
+                product = product,
+                onBuyClick = { onBuyClick(product) }
+            )
+        }
+    }
+}
+@Composable
+//@Preview
+fun ProductGridPreview() {
+
+    val sampleProducts = listOf(
+        Product(
+            imageUrl = "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+            title = "Iphone",
+            price = 19.2
+        ),
+        Product(
+            imageUrl = "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+            title = "Samsung Galaxy",
+            price = 29.99
+        ),
+        Product(
+            imageUrl = "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+            title = "Samsung Galaxy",
+            price = 29.99
+        ),
+        Product(
+            imageUrl = "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+            title = "Samsung Galaxy",
+            price = 29.99
+        ),
+        Product(
+            imageUrl = "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+            title = "Samsung Galaxy aaaaaadasdasdaddsda",
+            price = 29.99
+        ),
+        Product(
+            imageUrl = "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+            title = "Samsung Galaxy",
+            price = 29.99
+        ),
+        Product(
+            imageUrl = "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+            title = "Samsung Galaxy",
+            price = 29.99
+        ),
+        Product(
+            imageUrl = "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+            title = "Samsung Galaxy",
+            price = 29.99
+        ),
+        Product(
+            imageUrl = "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+            title = "Samsung Galaxy",
+            price = 29.99
+        )
+
+    )
+
+    ProductGrid(
+        products = sampleProducts,
+        onBuyClick = { /* Acción de compra */ }
+    )
+}
