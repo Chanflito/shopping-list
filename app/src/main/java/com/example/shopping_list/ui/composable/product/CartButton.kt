de8725dbf53befaf7757a9cfcf660994b54ea92a
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-08-13 18:10:33 -0300

    style: modify button color and background

--- app/src/main/java/com/example/shopping_list/ui/composable/product/CartButton.kt
+++ app/src/main/java/com/example/shopping_list/ui/composable/product/CartButton.kt
@@ -1,5 +1,6 @@
 package com.example.shopping_list.ui.composable.product
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
@@ -11,14 +12,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.shopping_list.ui.theme.Blue40
 
 @Composable
 fun CartButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit,
-    containerColor: Color = Blue40,
-    contentColor: Color = Color.White,
+    borderColor: Color = Blue40,
+    iconColor: Color = Blue40,
+    containerColor: Color = Color.White,
     iconSize: Dp
 ) {
     Button(
@@ -26,10 +29,15 @@ fun CartButton(
         modifier = modifier
             .wrapContentHeight(),
         colors = ButtonDefaults.buttonColors(
-            containerColor,
-            contentColor
-        )
+            containerColor = containerColor,
+            contentColor = iconColor
+        ),
+        border = BorderStroke(2.dp, borderColor) // Define el grosor y color del borde
     ) {
-        Icon(imageVector = Icons.Filled.ShoppingCart, contentDescription = "", Modifier.size(iconSize))
+        Icon(
+            imageVector = Icons.Filled.ShoppingCart,
+            contentDescription = "",
+            modifier = Modifier.size(iconSize)
+        )
     }
 }
