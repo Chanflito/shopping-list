5991e0453878c8e1d1be3470f66f64f726cf575b
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-09-26 22:46:59 -0300

    add order dao entity

--- app/src/main/java/com/example/shopping_list/data/Entity.kt
+++ app/src/main/java/com/example/shopping_list/data/Entity.kt
@@ -1,7 +1,10 @@
 package com.example.shopping_list.data
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
+import java.util.Date
 
 @Entity(tableName = "cart")
 data class CartProduct(
@@ -20,4 +23,24 @@ data class FavoriteProduct(
     val price: Double,
     val title: String,
     val description: String
+)
+
+/**
+ *This represents the "shopping list" where each item can have multiple products based on the cart
+ **/
+@Entity(tableName = "orders")
+data class Order(
+    @PrimaryKey(autoGenerate = true) val orderId: Int = 0,
+    val orderDate: Long
+)
+
+@Entity(tableName = "order_items")
+data class OrderItem(
+    @PrimaryKey(autoGenerate = true) val itemId: Int = 0,
+    val orderId: Int,
+    val productId: Int,
+    val quantity: Int,
+    val price: Double,
+    val title: String,
+    val image: String
 )
