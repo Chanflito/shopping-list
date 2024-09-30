74a9f7628fbfa1b19ed424056a7ffeab7de48958
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-09-29 23:54:25 -0300

    remove unused imports

--- app/src/main/java/com/example/shopping_list/nav/TopNavigationBar.kt
+++ app/src/main/java/com/example/shopping_list/nav/TopNavigationBar.kt
@@ -25,17 +25,13 @@ import androidx.compose.material3.TextFieldDefaults
 
 import androidx.compose.runtime.Composable
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.example.shopping_list.R
 
